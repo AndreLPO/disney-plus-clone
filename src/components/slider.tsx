fb70b99c4b391891cdcff63a69a8467b3fcdf7d0
Andre Oliveira <69281348+AndreLPO@users.noreply.github.com>
2022-02-01 20:41:47 -0300

    Alterado favicon e titulo da página, modelado o slider

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import SliderItem from "./slider-item";
 import "../styles/slider.css";
+import direita from "../assets/icons/seta_direita.svg";
+import esquerda from "../assets/icons/seta_esquerda.svg";
 
 export default function SliderComponent() {
   return (
-    <div className="slider">
-      <SliderItem />
-      <SliderItem />
-      <SliderItem />
-      <SliderItem />
-      <SliderItem />
-      <SliderItem />
+    <div className="visivel">
+      <div className="fundo-seta">
+        <img src={esquerda} alt="Seta de navegação" className="seta esquerda" />
+        <img src={direita} alt="Seta de navegação" className="seta direita" />
+      </div>
+      <div className="slider" id="slide">
+        <SliderItem />
+        <SliderItem />
+        <SliderItem />
+        <SliderItem />
+        <SliderItem />
+        <SliderItem />
+      </div>
     </div>
   );
 }
